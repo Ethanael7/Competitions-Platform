4e35602fd116e66258f744fe7d65a6fb2353c7a1
Ethanael7 <123254382+Ethanael7@users.noreply.github.com>
2024-09-25 15:41:27 -0400

    Create competioncontroller.py

---
+++ App/controllers/competioncontroller.py
@@ -0,0 +1,31 @@
+import csv
+from models import Competition
+
+class CompetitionController:
+    def __init__(self):
+        self.competitions = []
+
+    def create_competition(self, name,date):
+        competition = Competition(name,date)
+        self.competitions.append(competition)
+
+ 
+    def view_competitions(self):
+        return [comp.get_competition_info() for comp in self.competitions]
+    
+    def view_competition_results(self, competition_name):
+        competition = self.get_competition_by_name(competition_name)
+        if competition:
+            return competition.get_competition_info()
+        return f"No competition named {competition_name}"
+    
+    def get_competition_name(self, name):
+        for competition in self.competitions:
+            if competition.name == name:
+                return competition
+            return None
+        
+        
+        
+
+    
